514a6351405e0f82d37aa1018fcd513908cad411
Paul TCHAA <paultchaa@gmail.com>
2019-06-23 12:12:51 +0100

    Automation with npm Scripts, pre/post hooks, managing parallel tasks with npm-run-all --parallel

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+var chalk = require('chalk');
+
+// chalk est la librairie permettant de specifier
+// la couleur du rendu affiche sur la console
+
+console.log(chalk.green('Starting app in dev mode...'));
